fdcfee9715e6eeb7c73c3dbf7ad4572ef30fa256
krishnakushare <krishkusharekk@gmail.com>
2018-08-29 16:00:36 +0530

    new project

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AdminLoginComponent } from './adminPortal/admin-login/admin-login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminLoginComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
